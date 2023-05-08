ee71d305743f3657da55b8e61e39fec94749ea27
martin-yeo <sangsooyeo@gmail.com>
2023-05-08 16:26:07 +0900

    슬라이드쇼 업그레이드

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/-3ei9pak)
 # 이미지 갤러리 만들기
 
-> 작성자 : (이름 쓰세요)
+> 작성자 : 여상수
 
 - 프로그래머스 강의 원본
   - [자바스크립트와 웹 프론트엔드 <파트6. 실습>](https://school.programmers.co.kr/learn/courses/10/10-%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8%EC%99%80-%EC%9B%B9-%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%94%EB%93%9C)
---
+++ script.js
@@ -0,0 +1,60 @@
+var req = new XMLHttpRequest();
+req.open("GET", "./json/image_list.json");
+req.onreadystatechange = function() {
+    if (this.readyState == 4) {
+        // console.log(this.response);
+        var data = JSON.parse(this.response);
+        for(var i=0; i < data.length; i++) {
+            var div = document.createElement("div");
+            div.setAttribute("class", "image");
+            div.onclick = function() {
+                this.classList.toggle("image-selected");
+            }
+            div.onmouseover = function() {
+                var element = this;
+                this.timerID = setTimeout( function() {
+                    element.classList.add("image-magnified");
+                }, 500);
+            }
+            div.onmouseout = function() {
+                clearTimeout(this.timerID);
+                this.classList.remove("image-magnified");
+            }
+            var img = document.createElement("img");
+            img.src = data[i];
+            div.appendChild(img);
+            document.body.appendChild(div);
+        }
+    }
+}
+req.send();
+
+function selectAll(btn) {
+    var images = document.querySelectorAll(".image");
+ 
+    if(btn.textContent == "Select All") {
+        for(var i=0; i < images.length; i++) {
+            images[i].classList.add("image-selected");
+        }
+        btn.textContent = "Unselect All"
+    } else {
+        for(var i=0; i < images.length; i++) {
+            images[i].classList.remove("image-selected");
+        }
+        btn.textContent = "Select All"
+    }
+}
+
+function slideShow(btn) {
+    var images = document.querySelectorAll(".image");
+    var index = 0;
+    images[index].classList.add("image-slideshow");
+    var intervalID = setInterval(function() {
+        images[index++].classList.remove("image-slideshow");
+        if(index < images.length) {
+            images[index].classList.add("image-slideshow");
+        } else {
+            clearInterval(intervalID);
+        }
+    }, 1000);
+}
